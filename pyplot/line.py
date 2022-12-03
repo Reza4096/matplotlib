71abb771a5ffeed6648a675b3f796c1c16bea46f
reza4096 <reza4096@yahoo.com>
2022-12-03 15:48:21 +0330

    Default X-Points

--- pyplot/line.py
+++ pyplot/line.py
@@ -16,3 +16,10 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(xpoints, ypoints)
 plt.show()
+
+# Plotting without x-points:
+# If we do not specify the points on the x-axis,
+# they will get the default values 0, 1, 2, 3 ,... depending on the length of the y-points.
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+plt.plot(ypoints)
+plt.show()
