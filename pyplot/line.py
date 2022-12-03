bbb9e2070eb2a318dfcd0b3aa1234e6e608757f4
reza4096 <reza4096@yahoo.com>
2022-12-03 15:34:56 +0330

    line

---
+++ pyplot/line.py
@@ -0,0 +1,10 @@
+# Draw a line in a diagram from position (0,0) to position (6,250):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
