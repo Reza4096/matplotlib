03a85cfa7e2f56201f28646f4305c293c6c9e35a
reza4096 <reza4096@yahoo.com>
2022-12-03 15:41:27 +0330

    Multiple Points

--- pyplot/line.py
+++ pyplot/line.py
@@ -8,3 +8,11 @@ ypoints = np.array([0, 250])
 
 plt.plot(xpoints, ypoints)
 plt.show()
+
+# Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10):
+# You can plot as many points as you like, just make sure you have the same number of points in both axis.
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
